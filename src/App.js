7947178417e2a6a4533a4f8ffa67426d85a434b2
Theng <thengvlog@gmail.com>
2018-09-22 10:12:40 +0700

    Init project

---
+++ src/App.js
@@ -0,0 +1,31 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import FastImage from 'react-native-fast-image'
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Hello</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  }
+});
